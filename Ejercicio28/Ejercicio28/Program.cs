f984700aa584b88c2f9b55e69ac3dc94ffc29470
diego.taub <diego.taub@accenture.com>
2021-04-06 20:36:19 -0300

    Ejercicio28

---
+++ Ejercicio28/Ejercicio28/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número: ");
+            int año;
+            while (true)
+            {
+                if(Int32.TryParse(Console.ReadLine(), out año))
+                {
+                    break;
+                }
+                Console.Write("Número inválido. Ingréselo nuevamente: ");
+            }
+
+            if (DateTime.IsLeapYear(año))
+            {
+                Console.Write("El número ingresado coincide con un año bisiesto.");
+            }
+            else
+            {
+                Console.Write("El número ingresado no coincide con un año bisiesto.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
